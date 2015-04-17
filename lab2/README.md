918d3a447edd4a102b743415a598777ad3a4e36a
Szymon Matejczyk <smatejczyk@gmail.com>
2015-04-17 12:37:41 +0200

    Lab2 version beta

--- lab2/README.md
+++ lab2/README.md
@@ -2,7 +2,47 @@
 
 ## Naive version
 
-## Using CUDA profiler
+In naive implementation every thread computes one element of resulting matrix (see `matrixMulNaive.cu`).
+
+```cuda
+// A, B - square matrices of size MATRIX_DIM * MATRIX_DIM
+#define MATRIX_DIM 256
+
+#define POS(i, j) (((i) * MATRIX_DIM) + j)
+
+/**
+ * Matrix multiplication: C = A * B
+ */
+__global__ void matrixMulCUDA(float *C, float *A, float *B)
+{
+    int posx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    int posy = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+
+    float res = 0.0;
+    for (int i = 0; i < MATRIX_DIM; ++i) {
+        res += A[POS(posx, i)] * B[POS(i, posy)];
+    }
+    // Write the block sub-matrix to device memory;
+    // each thread writes one element
+    C[POS(posx, posy)] = res;
+}
+```
+
+## CUDA profiler
+
+CUDA Visual Profiler is a tool to profile CUDA applications written in any language.
+
+We can use it to profile `matrixMulNaive` program.
+
+Run command line profiler on the CUDA server using `nvprof ./matrixMulNaive` to obtain summary of the run. 
+
+We can also use it to generate a trace for detailed analysis:
+```
+nvprof --analysis-metrics -o  mm-analysis.nvprof ./matrixMulNaive
+```
+Now, download `mm-analysis.nvprof` file and analyse it using visual profiler (`nvvp`).
+
+Can you tell what is the problem with the naive version of matrix multiplication?
 
 ## Tiled matrix multiplication (2 points)
 
@@ -21,12 +61,13 @@ Algorithm:
   1. Load that tile to the shared memmory (each thread loads 1 element)
   2. Each thread computes part of the sum from the loaded tile
 
-**Implement the algorithm using CUDA platform.**
+**Implement the algorithm for square matrices using CUDA platform.**
 
 You can assume that matrix dimensions are multiplications of tiles' dimensions.
 
 ### Example
 When `A = B`, values of `C` in red tile `(2, 2)` depend only on tiles in grey (dark blue element depends on green elements).
+
 ![Observation](https://raw.githubusercontent.com/szymonm/pwir-cuda-labs/master/lab2/graphics/tiledMM0.png)
 
 **Step 1** Download tiles `(0, 2)` and `(2, 0)` to shared memory and compute parts of sums in `(2, 2)` from this tiles.
@@ -41,3 +82,6 @@ When `A = B`, values of `C` in red tile `(2, 2)` depend only on tiles in grey (d
 
 ![Step 3](https://raw.githubusercontent.com/szymonm/pwir-cuda-labs/master/lab2/graphics/tiledMM3.png)
 ![Step 4](https://raw.githubusercontent.com/szymonm/pwir-cuda-labs/master/lab2/graphics/tiledMM4.png)
+
+## Extensions
+
---
+++ lab2/matrixMulNaive.cu
@@ -0,0 +1,146 @@
+/**
+ * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+
+// System includes
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define BLOCK_SIZE 16
+#define MATRIX_DIM 256
+
+#define POS(i, j) (((i) * MATRIX_DIM) + j)
+
+#define cudaCheck(error) \
+  if (error != cudaSuccess) { \
+    printf("Fatal error: %s at %s:%d\n", \
+      cudaGetErrorString(error), \
+      __FILE__, __LINE__); \
+    exit(1); \
+  }
+
+/**
+ * Matrix multiplication (CUDA Kernel) on the device: C = A * B
+ * wA is A's width and wB is B's width
+ */
+__global__ void matrixMulCUDA(float *C, float *A, float *B)
+{
+    int posx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    int posy = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+
+    float res = 0.0;
+    for (int i = 0; i < MATRIX_DIM; ++i) {
+        res += A[POS(posx, i)] * B[POS(i, posy)];
+    }
+    // Write the block sub-matrix to device memory;
+    // each thread writes one element
+    C[POS(posx, posy)] = res;
+}
+
+void randomInit(float *data, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        data[i] = ((double)rand()/(double)RAND_MAX);
+    }
+}
+
+double computeMatrixMulPos(int row, int column, float* A, float* B) {
+  double sum = 0.0;
+  for (int i = 0; i < MATRIX_DIM; i++) {
+    sum += A[POS(row, i)] * B[POS(i, column)];
+  }
+  return sum;
+}
+
+/**
+ * Run a simple test of matrix multiplication using CUDA
+ */
+int main()
+{
+    // Allocate host memory for matrices A, B and C
+    size_t matrix_mem_size = sizeof(float) * MATRIX_DIM * MATRIX_DIM;
+    float *h_A = (float *)malloc(matrix_mem_size);
+    float *h_B = (float *)malloc(matrix_mem_size);
+    float *h_C = (float *) malloc(matrix_mem_size);
+
+    if (h_A == NULL || h_B == NULL || h_C == NULL)
+    {
+        fprintf(stderr, "Failed to allocate host matrices!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize host memory
+    randomInit(h_A, MATRIX_DIM * MATRIX_DIM);
+    randomInit(h_B, MATRIX_DIM * MATRIX_DIM);
+
+    // Allocate device memory
+    float *d_A, *d_B, *d_C;
+
+    cudaCheck(cudaMalloc((void **) &d_A, matrix_mem_size));
+    cudaCheck(cudaMalloc((void **) &d_B, matrix_mem_size));
+    cudaCheck(cudaMalloc((void **) &d_C, matrix_mem_size));
+
+    // copy host memory to device
+    cudaCheck(cudaMemcpy(d_A, h_A, matrix_mem_size, cudaMemcpyHostToDevice));
+
+    cudaCheck(cudaMemcpy(d_B, h_B, matrix_mem_size, cudaMemcpyHostToDevice));
+
+    // Setup execution parameters
+    dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 grid(MATRIX_DIM / threads.x, MATRIX_DIM / threads.y);
+
+    // Create and start timer
+    printf("Computing result using CUDA Kernel...\n");
+
+    // Performs warmup operation using matrixMul CUDA kernel
+    matrixMulCUDA<<< grid, threads >>>(d_C, d_A, d_B);
+    
+    cudaCheck(cudaPeekAtLastError());
+    
+
+    // Copy result from device to host
+    cudaCheck(cudaMemcpy(h_C, d_C, matrix_mem_size, cudaMemcpyDeviceToHost));
+
+    printf("Checking computed result for correctness: ");
+    bool correct = true;
+
+    // test relative error by the formula
+    //     |<x, y>_cpu - <x,y>_gpu|/<|x|, |y|>  < eps
+    double eps = 1.e-3; // machine zero
+
+    for (int row = 0; row < MATRIX_DIM; row++)
+        for (int column = 0; column < MATRIX_DIM; column++) {
+            double expected = computeMatrixMulPos(row, column, h_A, h_B);
+            if (abs(h_C[POS(row,column)] - expected) > eps) {
+                printf("ERROR: position (%d, %d) %f != %f\n", row, 
+                    column, h_C[POS(row, column)], expected);
+                correct = false;
+            }
+        }
+
+    printf("%s\n", correct ? "Result = PASS" : "Result = FAIL");
+
+    // Clean up memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    if (correct) {
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+}
